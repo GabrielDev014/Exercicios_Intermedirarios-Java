c5e74afe171843e16f65455c34796629e66fb55c
Gabriel Lima <gabriellimaalves.si25@gmail.com>
2024-08-26 16:24:35 -0300

    Create Pessoas.java

---
+++ Polimorfismo/GerarSeresVivos/Pessoas.java
@@ -0,0 +1,29 @@
+package gerarseresvivospolimorfismodois;
+
+public class Pessoas extends SeresVivos {
+private int anoNascimento;
+
+public Pessoas(int anoNascimento, String nome, int idade){
+    super(nome, idade);
+    this.anoNascimento = anoNascimento;
+}
+
+public int getAnoNascimento(){
+    return anoNascimento;
+}
+
+public void setAnoNascimento(int anoNascimento){
+    this.anoNascimento = anoNascimento;
+}
+
+@Override
+public String toString(){
+    return "Pessoas {" + "ano do nascimento = " + anoNascimento + '}' + super.toString();
+}
+
+@Override
+public void gerarIdade(int anoAtual){
+    this.setIdade(anoAtual - getAnoNascimento());
+}
+
+}
